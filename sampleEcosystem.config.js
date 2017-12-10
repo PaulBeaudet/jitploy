d8bd40ba1a247300b5467ffbc2662a770a80ec11
Paul Beaudet <inof8or@gmail.com>
2017-12-10 13:31:30 -0500

    Add an example configuration file that can be used with pm2

---
+++ sampleEcosystem.config.js
@@ -0,0 +1,15 @@
+// install pm2 with npm install -g pm2
+// pm2 is a tool to background and autostart processes. It can also keep track of logs and monitor memory usage
+// This is a configuration file to run this service when your own env variables are added
+// To create your own to modify that is ignored by git 'cp sampleEcosystem.config.js ecosystem.config.js'
+module.exports = {                                           // Run this config file with 'pm2 start sampleEcosystem.config.js'
+  apps : [{                                                  // this is an array that could include multiple services
+    name   : "jitploy",                                      // name that shows on using 'pm2 status'
+    script : "./jitploy.js",                                 // relitive executable assuming cofiguration is in same folder as service
+    watch  : true,                                           // service will restart when script file is changed
+    env    : {
+      "MONGODB_URI": "address to database push credentials", // this database could be run locally as well as above options
+      "PORT": 3000                                           // port of address that gets post requested to
+    }
+  }]
+};
